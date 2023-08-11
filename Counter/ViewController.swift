4395dcc9c8b8f4e61431934d76427c6c9e56517f
ValeriiaPetrosian <petrosyan.valeria@gmail.com>
2023-08-11 19:22:24 +0400

    Создание счетчика

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var NumberView: UILabel!
+    
+    @IBOutlet weak var PlusButton: UIButton!
+    
+   private var Number = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        NumberView.text = "Значение счетчика: \n\(Number)"
     }
-
-
+       
+    @IBAction func PlusButtonTap(_ sender: Any) {
+        Number += 1
+        NumberView.text = "Значение счетчика: \n\(Number)"
+    }
 }
+    
+
 
